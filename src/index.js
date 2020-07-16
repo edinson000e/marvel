c0af90b2c1018abc486f46d4ee2a5fabf95ee4d2
Maryury <edinsonvillazana@gmail.com>
2020-07-15 21:34:25 -0400

    create context provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { StateProvider } from "./store";
+import { mainReducer } from "./reducers";
+import { initialState } from "./store/initialState";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={mainReducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+export const mainReducer = ({}, action) => {
+  return {};
+};
