ecd923e47f90c163c430e334b907e21b008ad556
Maryury <edinsonvillazana@gmail.com>
2020-07-17 21:08:13 -0400

    add styles in body

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import { Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  Router,
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter
+} from "react-router-dom";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Characteres from "./components/characters";
 import history from "./store/browserHistory";
-function App() {
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path="/" render={props => <Characteres {...props} />} />
+import LightTheme from "./themes/light";
+import DarkTheme from "./themes/dark";
 
-        <Redirect from="*" to="/" />
-      </Switch>
-    </Router>
+const GlobalStyle = createGlobalStyle`
+	body{
+		background: ${p => p.theme.bodyBackgroundColor};
+		min-height: 100vh;
+		margin: 0;
+		color: ${p => p.theme.bodyFontColor};
+		font-family: 'Kaushan Script'
+	}
+`;
+
+function App() {
+  const [theme, setTheme] = useState(LightTheme);
+  return (
+    <ThemeProvider
+      theme={{
+        ...theme,
+        setTheme: () => {
+          setTheme(s => (s.id === "light" ? DarkTheme : LightTheme));
+        }
+      }}
+    >
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={props => <Characteres {...props} />} />
+
+          <Redirect from="*" to="/" />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
