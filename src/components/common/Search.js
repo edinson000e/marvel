c4648e5c42bd70d6c53de2bb3c1822c73d219503
Maryury <edinsonvillazana@gmail.com>
2020-07-18 00:41:34 -0400

    create icon search

---
+++ src/components/common/Search.js
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  outline: none;
+  border: none;
+  color: white;
+  background-color: red;
+`;
+
+export const Search = () => {
+  return <Button type="submit">icono</Button>;
+};
