4c419835e6e630552247c65dc07e8896fe10a788
Maryury <edinsonvillazana@gmail.com>
2020-07-30 04:16:11 -0400

    add localstorage in comic

--- src/pages/search/comics.js
+++ src/pages/search/comics.js
@@ -1,78 +1,71 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import {
-  PageLayout,
   ContainerLoading,
-  Breadcrumb,
   TitleDescription,
   Grid,
   Card
 } from "../../components/common";
 import { Container } from "../../components/common/Search";
-import { searchComics, resetComics } from "../../actions/comics";
 
+import { reset } from "../../actions/global";
+
+import { useHistory } from "react-router-dom";
 import { useStateValue } from "../../store";
 import Modal from "../characters/modalDetails";
-import { useHistory } from "react-router-dom";
-import { useLocalStorage } from "../../customHook/useLocalStorage";
-const SearchComics = props => {
-  const [{ comics }, dispatch] = useStateValue();
-  const [searchComicsLocal, setSearchComicsLocal] = useLocalStorage(
-    "searchComicsLocal",
-    []
-  );
-  const history = useHistory();
-  let param = props.match.params.id;
-  const initFetch = useCallback(() => {
-    searchComics(dispatch, param);
-  }, [dispatch, param]);
+import { useLocalStorageSearch } from "../../customHook/useLocalStorage";
 
-  const initResetFetch = useCallback(() => {
-    dispatch(resetComics());
-  }, [dispatch]);
+const SearchComics = props => {
+  const [{ global }, dispatch] = useStateValue();
+  const history = useHistory();
+  let ParamID = props.match.params.id.toLowerCase();
+  let url = `/v1/public/comics?titleStartsWith=${ParamID}`;
+  const searchComicsData = useLocalStorageSearch("searchComics", url, ParamID);
 
   useEffect(() => {
-    initFetch();
     return () => {
-      initResetFetch();
+      dispatch(reset());
     };
-  }, [initFetch, initResetFetch]);
+  }, [url]);
 
   return (
     <Container>
-      {comics.isFetching ? (
+      {global.isFetching ? (
         <ContainerLoading />
-      ) : comics.complete && comics.count === 0 ? (
-        <>
-          <TitleDescription dark>
-            Sorry! We couldn't find any results to "{props.match.params.id}"{" "}
-          </TitleDescription>{" "}
-          <h2>
-            If you're not happy with the results, please do another search
-          </h2>
-        </>
       ) : (
-        <>
-          <Grid>
-            {comics.results.length > 0 &&
-              comics.results.map((value, index) => {
-                let title = value.title;
-                return (
-                  <Card
-                    key={index}
-                    title={title}
-                    photo={
-                      value.thumbnail.path + "." + value.thumbnail.extension
-                    }
-                    description={value.description}
-                    onClick={() => {
-                      history.push(`/comic/${value.id}`);
-                    }}
-                  />
-                );
-              })}
-          </Grid>
-          <Modal />
-        </>
+        global.success &&
+        (searchComicsData.total === 0 ? (
+          <>
+            <TitleDescription dark>
+              Sorry! We couldn't find any results to "{props.match.params.id}"{" "}
+            </TitleDescription>{" "}
+            <h2>
+              If you're not happy with the results, please do another search
+            </h2>
+          </>
+        ) : (
+          <>
+            <Grid>
+              {searchComicsData.results.length > 0 &&
+                searchComicsData.results.map((value, index) => {
+                  let title = value.title;
+                  return (
+                    <Card
+                      key={index}
+                      title={title}
+                      photo={
+                        value.thumbnail.path + "." + value.thumbnail.extension
+                      }
+                      description={value.description}
+                      onClick={() => {
+                        history.push(`/comic/${value.id}`);
+                      }}
+                    />
+                  );
+                })}
+            </Grid>
+            <Modal />
+          </>
+        ))
       )}
     </Container>
   );
